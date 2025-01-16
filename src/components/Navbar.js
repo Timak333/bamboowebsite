8612a1385bbd2d70bee3d1dab15ce300b1bf6b86
Timak333 <90118214+Timak333@users.noreply.github.com>
2025-01-15 20:50:55 -0500

    Pages for navigation bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar( {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#Home"></a></li>
+
+            </ul>
+
+
+        </nav>
+    )
+})
