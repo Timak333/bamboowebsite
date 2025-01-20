e10224866c91ed034bf94cb28b89484e63d94a5b
Timak333 <90118214+Timak333@users.noreply.github.com>
2025-01-20 16:44:17 -0500

    working on burger menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,17 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import websiteLogo from '../../assets/Images/websiteLogo.png';
+import { slide as Menu } from 'react-burger-menu';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
                 <Link to="/" className="logoLink">
                     <img src={websiteLogo} alt="Logo" className="logo" />
                 </Link>
+                <div className="navlinks">
                 <Link to="/">Home</Link>
                 <Link to="/MosoBamboo">Moso Bamboo</Link>
                 <Link to="/Gallery">Gallery</Link>
                 <Link to="/EcoCalculator">EcoCalculator</Link>
                 <Link to="/About">About</Link>
+                </div>
+                <Menu right>
+                    <Link to="/" className="menu-item">Home</Link>
+                    <Link to="/MosoBamboo" className="menu-item">Moso Bamboo</Link>
+                    <Link to="/Gallery" className="menu-item">Gallery</Link>
+                    <Link to="/EcoCalculator" className="menu-item">EcoCalculator</Link>
+                    <Link to="/About" className="menu-item">About</Link>
+                </Menu>
         </nav>
     )
 }
