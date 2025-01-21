6d5e2c4199ad59c2a106e615415bd0490090c0ef
Timak333 <90118214+Timak333@users.noreply.github.com>
2025-01-20 20:55:11 -0500

    installed material ui icons

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,30 +1,56 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import websiteLogo from '../../assets/Images/websiteLogo.png';
-import { slide as Menu } from 'react-burger-menu';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavBar = () => {
+        setIsOpen(!isOpen);
+    }
     return (
-        <nav className="navbar">
-                <Link to="/" className="logoLink">
-                    <img src={websiteLogo} alt="Logo" className="logo" />
-                </Link>
-                <div className="navlinks">
+        <>
+           <nav className="navbar">
+            <Link to="/" className="logoLink">
+                        <img src={websiteLogo} alt="Logo" className="logo" />
+                    </Link>
+                <div className="navbarLinks">
                 <Link to="/">Home</Link>
                 <Link to="/MosoBamboo">Moso Bamboo</Link>
                 <Link to="/Gallery">Gallery</Link>
                 <Link to="/EcoCalculator">EcoCalculator</Link>
                 <Link to="/About">About</Link>
                 </div>
-                <Menu right>
-                    <Link to="/" className="menu-item">Home</Link>
-                    <Link to="/MosoBamboo" className="menu-item">Moso Bamboo</Link>
-                    <Link to="/Gallery" className="menu-item">Gallery</Link>
-                    <Link to="/EcoCalculator" className="menu-item">EcoCalculator</Link>
-                    <Link to="/About" className="menu-item">About</Link>
-                </Menu>
-        </nav>
-    )
-}
+                {/* Navigtion for full desktop screen */}
+                <div className="navLinks">
+                    <Link to="/">Home</Link>
+                    <Link to="/MosoBamboo">Moso Bamboo</Link>
+                    <Link to="/Gallery">Gallery</Link>
+                    <Link to="/EcoCalculator">EcoCalculator</Link>
+                    <Link to="/About">About</Link>
+                </div>
+                {/* Toggle Navigation for mobile} */}
+                <div className="toggleBtn">
+                    <button onClick={toggleNavBar}>
+                        {isOpen ? <CloseIcon /> : <MenuIcon />}
+                    </button>
+                </div>
+            </nav>
+            {/* Navigation for mobile */}
+            {isOpen && (
+                <div className="mobileNavLinks">
+                    <Link to="/" onClick={toggleNavBar}>Home</Link>
+                    <Link to="/MosoBamboo" onClick={toggleNavBar}>Moso Bamboo</Link>
+                    <Link to="/Gallery" onClick={toggleNavBar}>Gallery</Link>
+                    <Link to="/EcoCalculator" onClick={toggleNavBar}>EcoCalculator</Link>
+                    <Link to="/About" onClick={toggleNavBar}>About</Link>
+                </div>
+            )}
+        </>
+    );
+};
+
 export default Navbar;
