03339a5a4f08287ad01e5fe09c666e7f4bd6fe72
Timak333 <90118214+Timak333@users.noreply.github.com>
2025-02-26 01:13:48 -0500

    add pie chart

--- frontend/src/Pages/ResultsPage.js
+++ frontend/src/Pages/ResultsPage.js
@@ -3,6 +3,10 @@ import { useLocation, useNavigate } from "react-router-dom";
 import './ResultPage.css';
 import resultBackground from '../assets/Images/resultBackground.png';
 import { Button, Card, CardContent } from '@mui/material';
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip);
 
 const ResultsPage = () => {
     const location = useLocation();
@@ -16,17 +20,32 @@ const ResultsPage = () => {
             </div>
         )
     }
+    const data = {
+        labels: ["Material Emissions", "Transport Emissions", "Energy Emissions"],
+        datasets: [
+            {
+                data: [
+                emissionsData.material_emissions, emissionsData.transport_emissions, emissionsData.energy_emissions],
+                backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+                hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+            },
+        ],
+    }
     return (
         <div className="backgroundImage" style={{ backgroundImage: `url(${resultBackground})`}}>
         <div className="resultsContainer">
             <Card className="emissionsBox">
                 <CardContent>
                 <h1>Carbon Emission Results</h1>
+                <div className="chartContainer">
+                <Pie data={data}/>
+                </div>
                 <p><strong>Material Emissions:</strong> {emissionsData.material_emissions.toFixed(2)} kg CO₂</p>
                 <p><strong>Transport Emissions:</strong> {emissionsData.transport_emissions.toFixed(2)} kg CO₂</p>
                 <p><strong>Energy Emissions:</strong> {emissionsData.energy_emissions.toFixed(2)} kg CO₂</p>
                 <hr />
                 <h2><strong>Total emissions: </strong>{emissionsData.total_emissions.toFixed(2)} kg CO₂</h2>
+                
                 </CardContent>
             </Card>
             </div>
