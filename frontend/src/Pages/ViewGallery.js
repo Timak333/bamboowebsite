8f02c5c4476962679f296f9419d42c70f15f5e5c
Timak333 <90118214+Timak333@users.noreply.github.com>
2025-04-17 03:26:41 -0400

    button styling

--- frontend/src/Pages/ViewGallery.js
+++ frontend/src/Pages/ViewGallery.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Slider from 'react-slick';
-import Flipcard from '../components/Flipcard.js';
+import Flipcard from '../components/Flipcard/Flipcard';
 import GalleryData from '../assets/Data/GalleryData';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
