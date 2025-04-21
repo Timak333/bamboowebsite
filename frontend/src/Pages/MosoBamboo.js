922787bf39ac2837769e1743454b42d055d07ccf
Timak333 <90118214+Timak333@users.noreply.github.com>
2025-04-21 01:31:13 -0400

    add essay and moso bamboo page

--- frontend/src/Pages/MosoBamboo.js
+++ frontend/src/Pages/MosoBamboo.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './MosoBamboo.css';
+import homeBackground3 from '../assets/Images/homeBackground3.png';
 
 const MosoBamboo = () => {
     return (
-        <div className="mosoBackground">
-            <h1>Welcome to the Moso bamboo page</h1>
+        <div className="backgroundImage" style={{ backgroundImage: `url(${homeBackground3})` }}>
+            <div className="mosoContainer">
+                <h1> Moso Bamboo Research</h1>
+                <iframe
+                src="/essays/thesisEssay.pdf"
+                className="mosoIframe"
+                title="Essay on Moso Bamboo"
+                />
+            </div>
         </div>
     );
-}
+};
 export default MosoBamboo;
